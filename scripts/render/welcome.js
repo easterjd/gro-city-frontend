e45c5f964c6c4ee51dbfd13a2ed9e8111b539e66
John Dillon Easter <johndilloneaster@gmail.com>
2018-08-07 10:18:59 -0700

    centered animation and added buttons

--- scripts/render/welcome.js
+++ scripts/render/welcome.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
+  setTimeout( () => {
+    document.querySelector('#fill').setAttribute("fill", "white")
+  }, 3000)
   const lineDrawing1 = anime({
     targets: '#lineDrawing .lines path',
     strokeDashoffset: [anime.setDashoffset, 0],
@@ -6,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     duration: 3000,
     delay: function(el, i) { return i * 600 },
     direction: 'alternate',
-    loop: true
+    loop: 1
   })
 
 })
