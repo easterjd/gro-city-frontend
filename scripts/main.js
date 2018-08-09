8d3d82fcf8639053dc57d70314a8b534979dc9b7
John Dillon Easter <johndilloneaster@gmail.com>
2018-08-09 15:28:34 -0700

    Added more styling to the login, welcome, and boards

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,15 @@ const authorize = require('./authorization/authorize');
 const renderPlants = require('./render/plants.js')
 const request = require('./requests/requests.js')
 
-if ( window.location.href === "http://127.0.0.1:8080/") {
+if ( window.location.href === "http://127.0.0.1:50104/") {
   document.location.replace('/index.html')
 }
+const lineDrawing1 = anime({
+  targets: '#lineDrawing .lines path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutExpo',
+  duration: 3000,
+  delay: function(el, i) { return i * 600 },
+  direction: 'alternate',
+  loop: true
+})
