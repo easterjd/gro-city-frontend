8db3e55f067109029273e135a37f1b77a40ee323
John Dillon Easter <johndilloneaster@gmail.com>
2018-08-09 22:44:23 -0700

    Added modal to login with more info

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,21 @@ const authorize = require('./authorization/authorize');
 const renderPlants = require('./render/plants.js')
 const request = require('./requests/requests.js')
 
-if ( window.location.href === "http://127.0.0.1:8080/") {
+if ( window.location.href === "http://127.0.0.1:61073/") {
   document.location.replace('/index.html')
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {
+      opacity: 0.7,
+      inDuration: 250,
+      outDuration: 250,
+      dismissible: true,
+      endingTop: '30%'
+    });
+  });
+
 const lineDrawing1 = anime({
   targets: '#lineDrawing .lines path',
   strokeDashoffset: [anime.setDashoffset, 0],
